f997060823fb97c13ca7e59c53a5a35b91bdc20a
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-25 08:28:16 -0600

    Middle of refactoring doing it in more object oriented way with Frame class containing roles and score

--- Bowling.Tests/BowlingTests.cs
+++ Bowling.Tests/BowlingTests.cs
@@ -67,191 +67,210 @@ namespace Bowling.Tests
             Assert.AreEqual(24, _game.Score());
         }
 
-        [Test]
-        public void Bowling_frame_for_no_role_is_0()
-        {
-            Assert.AreEqual(0, _game.Frame);
-        }
+        //[Test]
+        //public void Bowling_frame_for_no_role_is_0()
+        //{
+        //    Assert.AreEqual(0, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_frame_for_1_role_is_1()
-        {
-            _game.Roll(2);
+        //[Test]
+        //public void Bowling_frame_for_1_role_is_1()
+        //{
+        //    _game.Roll(2);
 
-            Assert.AreEqual(1, _game.Frame);
-        }
+        //    Assert.AreEqual(1, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_frame_for_2_role_is_1()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
+        //[Test]
+        //public void Bowling_frame_for_2_role_is_1()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
 
-            Assert.AreEqual(1, _game.Frame);
-        }
+        //    Assert.AreEqual(1, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_frame_for_3_role_is_2()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
+        //[Test]
+        //public void Bowling_frame_for_3_role_is_2()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
 
-            Assert.AreEqual(2, _game.Frame);
-        }
+        //    Assert.AreEqual(2, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_frame_for_4_role_is_2()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
+        //[Test]
+        //public void Bowling_frame_for_4_role_is_2()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
 
-            Assert.AreEqual(2, _game.Frame);
-        }
+        //    Assert.AreEqual(2, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_frame_for_5_role_is_2()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
+        //[Test]
+        //public void Bowling_frame_for_5_role_is_2()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
 
-            Assert.AreEqual(3, _game.Frame);
-        }
+        //    Assert.AreEqual(3, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_for_game_is_20_roles_for_10_frames_if_non_spare_or_strike_on_last_frame()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
+        //[Test]
+        //public void Bowling_for_game_is_20_roles_for_10_frames_if_non_spare_or_strike_on_last_frame()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
 
-            Assert.AreEqual(10, _game.Frame);
-        }
+        //    Assert.AreEqual(10, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_for_game_is_exception_if_over_20_roles_if_non_spare_or_strike_on_last_frame()
-        {
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
-            _game.Roll(2);
-            _game.Roll(3);
-            _game.Roll(7);
-            _game.Roll(1);
-            _game.Roll(0);
+        //[Test]
+        //public void Bowling_for_game_is_exception_if_over_20_roles_if_non_spare_or_strike_on_last_frame()
+        //{
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
+        //    _game.Roll(2);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(1);
+        //    _game.Roll(0);
         
-            var ex = Assert.Throws<Exception>(() => _game.Roll(0));
-            Assert.That(ex.Message, Is.EqualTo("Cannot have extra role on last frame if not a spare or a strike"));
-            //Assert.Throws<ArgumentException>(delegate { throw new ArgumentException(); });
-        }
+        //    var ex = Assert.Throws<Exception>(() => _game.Roll(0));
+        //    Assert.That(ex.Message, Is.EqualTo("Cannot have extra role on last frame if not a spare or a strike"));
+        //    //Assert.Throws<ArgumentException>(delegate { throw new ArgumentException(); });
+        //}
 
-        [Test]
-        public void Bowling_perfect_game_has_12_roles_and_10_frames()
-        {
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
+        //[Test]
+        //public void Bowling_perfect_game_has_12_roles_and_10_frames()
+        //{
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
 
-            Assert.AreEqual(10, _game.Frame);
-        }
+        //    Assert.AreEqual(10, _game.Frame);
+        //}
 
-        [Test]
-        public void Bowling_perfect_game_with_10_roles()
-        {
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
+        //[Test]
+        //public void Bowling_perfect_game_with_10_roles()
+        //{
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
 
 
-            Assert.AreEqual(260, _game.Score());
-        }
+        //    Assert.AreEqual(260, _game.Score());
+        //}
 
-        [Test]
-        public void Bowling_perfect_game_with_11_roles()
-        {
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
+        //[Test]
+        //public void Bowling_perfect_game_with_11_roles()
+        //{
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
      
 
-            Assert.AreEqual(290, _game.Score());
-        }
+        //    Assert.AreEqual(290, _game.Score());
+        //}
 
-        [Test]
-        public void Bowling_perfect_game_score()
-        {
-            _game.Roll(10);
-            _game.Roll(10); 
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
-            _game.Roll(10);
+        //[Test]
+        //public void Bowling_perfect_game_score()
+        //{
+        //    _game.Roll(10);
+        //    _game.Roll(10); 
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
 
-            Assert.AreEqual(300, _game.Score());
-        }
+        //    Assert.AreEqual(300, _game.Score());
+        //}
+
+        //[Test]
+        //public void Bowling_almost_perfect_game_with_last_frame_rolling_a_spare_score()
+        //{
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(10);
+        //    _game.Roll(3);
+        //    _game.Roll(7);
+        //    _game.Roll(10);
+
+        //    Assert.AreEqual(273, _game.Score());
+        //}
     }
 }
--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bowling
@@ -12,94 +13,157 @@ namespace Bowling
         private int? _previousPreviousRoleScore = null;
         private int _rolesInLastFrame = 0;
 
-        private const int RolesInFrame = 2;
+        private const int RolesInRegularFrame = 2;
         private const int TotalFramesInGame = 10;
         private const int MaxRolesForGame = 21;
         private const int TotalPins = 10;
 
-        public int Frame { get; set; }
+        private Frame[] _frames;
+        private int _currentFrameIndex;
+        private int _currentRollIndex;
 
+
+        public class Frame
+        {
+            public int[] Rolls = new int[2];
+            public int Score;
+        }
+
+        public class LastSpecialFrame : Frame
+        {
+            public int ExtraRole;
+        }
+
+
+        public Game()
+        {
+            _frames = new Frame[10] { new Frame(), new Frame(), new Frame(), new Frame(), new Frame(), new Frame(), new Frame(), new Frame(), new Frame(), new Frame() };
+        }
 
         public void Roll(int pins)
         {
-            _roleTotal++;
+            _frames[_currentFrameIndex].Rolls[_currentRollIndex] = pins;
+            _frames[_currentFrameIndex].Score = _currentRollIndex == 0 ? pins : _frames[_currentFrameIndex].Rolls.Sum();
 
-            if (_roleTotal == MaxRolesForGame && !Last2PinsScoreTotalIs(TotalPins))
+           _currentRollIndex = ++_currentRollIndex % RolesInRegularFrame; 
+
+            if(_currentFrameIndex > 0)
             {
-                throw new Exception("Cannot have extra role on last frame if not a spare or a strike");
-            }
-
-            if(Frame < TotalFramesInGame)
-            {
-                if (pins == TotalPins)
+                var previousFrame = _currentFrameIndex - 1;
+                if (_currentRollIndex == 1)
                 {
-                    Frame++;
+                    if (IsFrameSpare(previousFrame))
+                    {
+                        _frames[previousFrame].Score += pins;
+                    }
                 }
-                else if (_roleTotal % RolesInFrame != 0)
+                else
                 {
-                    Frame++;
-                }
-            }
-
-            if(Frame == TotalFramesInGame)
-            {
-                _rolesInLastFrame++;
-
-             
-            }
-
-
-            if (!(Frame == TotalFramesInGame && _rolesInLastFrame == 3))
-            {         
-                if (IsPreviousPreviousRoleStrike())
-                {
-                    _score += _previousRoleScore.Value + pins;
-                }
-                else if(IsLastRolesSpare())
-                {
-                    _score += pins;
+                    if (IsFrameStrike(previousFrame))
+                    {
+                        _frames[previousFrame].Score += _frames[_currentFrameIndex].Score;
+                    }
                 }
             }
-
-            _score += pins;
-
-            if (_roleTotal == 1)
+            if(_currentRollIndex == 0)
             {
-                _previousRoleScore = pins;
+                _currentFrameIndex++;
             }
 
-            if (_roleTotal > 1)
+            if (pins == TotalPins)
             {
-                _previousPreviousRoleScore = _previousRoleScore;
-                _previousRoleScore = pins;
+                _currentFrameIndex++;
+                _currentRollIndex = 0;
             }
+
+            //_roleTotal++;
+
+            //if (_roleTotal == MaxRolesForGame && !Last2PinsScoreTotalIs(TotalPins))
+            //{
+            //    throw new Exception("Cannot have extra role on last frame if not a spare or a strike");
+            //}
+
+            //if(_currentFrame < TotalFramesInGame)
+            //{
+            //    if (pins == TotalPins)
+            //    {
+            //        _currentFrame++;
+            //    }
+            //    else if (_roleTotal % RolesInFrame != 0)
+            //    {
+            //        _currentFrame++;
+            //    }
+            //}
+
+            //if(_currentFrame == TotalFramesInGame)
+            //{
+            //    _rolesInLastFrame++;
+
+
+            //}
+
+
+            //if (!(_currentFrame == TotalFramesInGame && _rolesInLastFrame == 3))
+            //{         
+            //    if (IsPreviousPreviousRoleStrike())
+            //    {
+            //        _score += _previousRoleScore.Value + pins;
+            //    }
+            //    else if(IsLastRolesSpare())
+            //    {
+            //        _score += pins;
+            //    }
+            //}
+
+            //_score += pins;
+
+            //if (_roleTotal == 1)
+            //{
+            //    _previousRoleScore = pins;
+            //}
+
+            //if (_roleTotal > 1)
+            //{
+            //    _previousPreviousRoleScore = _previousRoleScore;
+            //    _previousRoleScore = pins;
+            //}
         }
 
-        private bool IsNewFrame()
+        private bool IsFrameStrike(int previousFrame)
         {
-            return (_roleTotal % RolesInFrame == 0);
+            return _frames[previousFrame].Rolls.Where(r => r == TotalPins).Any();
         }
 
-        private bool IsPreviousPreviousRoleStrike()
+        private bool IsFrameSpare(int previousFrame)
         {
-            return _previousPreviousRoleScore.HasValue && _previousPreviousRoleScore.Value == TotalPins;
+            return !_frames[previousFrame].Rolls.Where(r => r == TotalPins).Any() && _frames[previousFrame].Score == TotalPins;
         }
 
-        private bool IsLastRolesSpare()
-        {
-            return  _roleTotal % RolesInFrame != 0
-                    && _previousPreviousRoleScore.HasValue && _previousRoleScore.HasValue 
-                    && ((_previousPreviousRoleScore.Value + _previousRoleScore.Value) == TotalPins);
-        }
+        //private bool IsNewFrame()
+        //{
+        //    return (_roleTotal % RolesInFrame == 0);
+        //}
 
-        private bool Last2PinsScoreTotalIs(int scoreTotal)
-        {
-            return _previousRoleScore.HasValue && _previousPreviousRoleScore.HasValue && ((_previousRoleScore.Value + _previousPreviousRoleScore.Value) == scoreTotal);
-        }
+        //private bool IsPreviousPreviousRoleStrike()
+        //{
+        //    return _previousPreviousRoleScore.HasValue && _previousPreviousRoleScore.Value == TotalPins;
+        //}
+
+        //private bool IsLastRolesSpare()
+        //{
+        //    return  _roleTotal % RolesInFrame != 0
+        //            && _previousPreviousRoleScore.HasValue && _previousRoleScore.HasValue 
+        //            && ((_previousPreviousRoleScore.Value + _previousRoleScore.Value) == TotalPins);
+        //}
+
+        //private bool Last2PinsScoreTotalIs(int scoreTotal)
+        //{
+        //    return _previousRoleScore.HasValue && _previousPreviousRoleScore.HasValue && ((_previousRoleScore.Value + _previousPreviousRoleScore.Value) == scoreTotal);
+        //}
 
         public int Score()
         {
-            return _score;
+            return _frames.Sum(f => f.Score);
         }
     }
 }
